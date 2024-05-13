8031f97ce9cc42d3519de00e1699f701765f8e64
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-13 19:09:34 +0530

    updated : code for parsing request body in backend

--- 1. Contact Manager App/server.js
+++ 1. Contact Manager App/server.js
@@ -5,6 +5,7 @@ import contactRouter from "./routes/contactRouter.js";
 
 const app=express()
 
+app.use(express.json())
 app.use("/api/contact",contactRouter)
 
 const PORT=process.env?.PORT || 3000
