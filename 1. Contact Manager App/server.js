5b4cffcf6c0c419d3a27c94e0b58b8a8e7f0d774
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-13 16:58:37 +0530

    updated : updated code for routers as middleware.

--- 1. Contact Manager App/server.js
+++ 1. Contact Manager App/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
+import contactRouter from "./routes/contactRouter.js";
 
 const app=express()
 
+app.use("/api/contact",contactRouter)
+
 const PORT=process.env?.PORT || 3000
+
 
 app.listen(PORT,()=>{
     console.log(`server is listening on port : ${PORT}`);
