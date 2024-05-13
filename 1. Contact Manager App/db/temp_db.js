05f574e2dce498e0afd3a455978b6024d1c672d3
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-13 18:46:36 +0530

    updated : code with getDataById() to fetch the data based on id.

--- 1. Contact Manager App/db/temp_db.js
+++ 1. Contact Manager App/db/temp_db.js
@@ -4,8 +4,13 @@ const getData=()=>{
     return db_map.size!==0?[...db_map.values()]:"no data found in db"
 }
 
+const getDataById=(key)=>{
+    const id=Number(key)
+    return db_map.size!==0?db_map.get(id):`no data found in db with an id : ${id}`
+}
+
 const setData=(key,value)=>{
     db_map.set(key,value)
 }
 
-export {getData,setData}
+export {getData,setData,getDataById}
