d24984e4cc55116c1aa1906fa40401f26546a7ab
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-14 16:22:36 +0530

    updated : added errorHandler as a middleware.

--- 1. Contact Manager App/server.js
+++ 1. Contact Manager App/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
 import contactRouter from "./routes/contactRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app=express()
 
 app.use(express.json())
 app.use("/api/contact",contactRouter)
+app.use(errorHandler)
 
 const PORT=process.env?.PORT || 3000
 
