bdab3f5342e8b5083aa7381aaf3fdddc3bceed0e
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-14 08:05:28 +0530

    updated : code with router for updateOldContact.

--- 1. Contact Manager App/routes/contactRouter.js
+++ 1. Contact Manager App/routes/contactRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {getAllContact,getContactById,createNewContact} from "../controllers/contactController.js";
+import {getAllContact,getContactById,createNewContact, updateOldContact} from "../controllers/contactController.js";
 
 const contactRouter=express.Router()
 
 contactRouter.get("/",getAllContact)
 contactRouter.get("/:id",getContactById)
 contactRouter.post("/",createNewContact)
+contactRouter.put("/:id",updateOldContact)
 
 export default contactRouter
