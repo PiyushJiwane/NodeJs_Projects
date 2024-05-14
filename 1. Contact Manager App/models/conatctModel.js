8c72667397cdd23720512c91db5ae5feb1485c25
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-14 20:20:09 +0530

    contactModel.js file used to create a schema and collection i.e. model in mongodb database.

---
+++ 1. Contact Manager App/models/conatctModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const contactSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"please add the contact name"]
+    },
+    email:{
+        type:String,
+        required:[true,"please add the email address"]
+    },
+    phone:{
+        type:String,
+        required:[true,"please add the phone number"]
+    }
+},{
+    timestamps:true
+})
+
+export default mongoose.model("contact",contactSchema)
