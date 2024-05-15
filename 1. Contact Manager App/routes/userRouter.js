ff5c675ed5c4b00a598092af1484412c4504e990
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-15 12:32:27 +0530

    updated : userRouter.get('/currentUser') updated with jwtTokenValidater middleware. This middleware will validate the token id.

--- 1. Contact Manager App/routes/userRouter.js
+++ 1. Contact Manager App/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {registration,login, currentUser} from "../controllers/userController.js";
+import jwtTokenValidater from "../middlewares/tokenValidateHandler.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/registration", registration)
 userRouter.post("/login", login)
-userRouter.get("/currentUser", currentUser)
+userRouter.get("/currentUser",jwtTokenValidater, currentUser)
 
 export default userRouter
