d9a239b2e4317c2b03575f7c42f977c8e1c11f38
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-15 12:33:56 +0530

    updated : jwtTokenValidater as middleware

--- 1. Contact Manager App/server.js
+++ 1. Contact Manager App/server.js
@@ -5,6 +5,7 @@ import contactRouter from "./routes/contactRouter.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import db from "./config/dbConnection.js";
 import userRouter from "./routes/userRouter.js";
+import jwtTokenValidater from "./middlewares/tokenValidateHandler.js";
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use("/api/contact", contactRouter)
 app.use("/api/user",userRouter)
 app.use(errorHandler)
+app.use(jwtTokenValidater)
 
 const PORT = process.env?.PORT || 3000
 
