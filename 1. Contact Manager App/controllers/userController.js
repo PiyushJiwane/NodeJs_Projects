208fe87fe2bb3a140faa445aee7713d196131bcd
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-15 11:15:18 +0530

    updated : login with jwt token

--- 1. Contact Manager App/controllers/userController.js
+++ 1. Contact Manager App/controllers/userController.js
@@ -1,10 +1,12 @@
 import expressAsyncHandler from "express-async-handler";
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
-const saltRound=10
+const SALT_ROUND=process.env.SALT_ROUND
+const JWT_SECRETE_KEY=process.env.JWT_SECRETE_KEY
 
-const registration=async (req,res)=>{
+const registration=expressAsyncHandler(async (req,res)=>{
     const { username, email, password } = req.body
 
     if (!username || !email || !password) {
@@ -12,22 +14,47 @@ const registration=async (req,res)=>{
         throw new Error("All fields are manditory")
     }
 
-    const bcrypt_password=await bcrypt.hash(password,saltRound)
+    const bcrypt_password=await bcrypt.hash(password,SALT_ROUND)
 
     const newUser = new userModel({
         username, email, password:bcrypt_password
     })
 
-    await newUser.save()
+    const user=await newUser.save()
 
-    res.status(201).json({
-        "data": "data got saved successfully !!!"
-    })
-}
+    if(user._id){
+        res.status(201).json({
+            "data": `data got saved successfully with an id : ${user._id}`
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid Data")
+    }
+})
 
-const login=(req,res)=>{
-    console.log("registration");
-}
+const login=expressAsyncHandler(async (req,res)=>{
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        res.status(400)
+        throw new Error("All fields are manditory")
+    }
+
+    const result=await userModel.findOne({email})
+
+    const hashedPassword=await bcrypt.compare(password,result.password)
+
+    if(email && hashedPassword){
+        const jwt_token=await jwt.sign({
+            _id:result._id,
+            u_name:result.username
+        },JWT_SECRETE_KEY)
+        res.status(200).json({jwt_token})
+    }else{
+        res.status(400)
+        throw new Error("email and password is incorrect !!!")
+    }
+})
 
 const currentUser=(req,res)=>{
     console.log("registration");
