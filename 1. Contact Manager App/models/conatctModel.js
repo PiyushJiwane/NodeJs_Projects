9cf48dda3caa4eaffba5b7e613f91e492194b23c
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-15 13:20:52 +0530

    updated : this code is making the relationship with user collection by using ref:'user'.

--- 1. Contact Manager App/models/conatctModel.js
+++ 1. Contact Manager App/models/conatctModel.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"user"
+    },
     name: {
         type: String,
         required: [true, "please add the contact name"]
