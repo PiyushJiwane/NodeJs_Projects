d02898dea892b526d7f2cdc405a33249e5072814
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-15 13:18:01 +0530

    formate document i.e refactored

--- 1. Contact Manager App/controllers/userController.js
+++ 1. Contact Manager App/controllers/userController.js
@@ -3,10 +3,10 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-const SALT_ROUND=process.env.SALT_ROUND
-const JWT_SECRETE_KEY=process.env.JWT_SECRETE_KEY
+const SALT_ROUND = Number(process.env.SALT_ROUND)
+const JWT_SECRETE_KEY = process.env.JWT_SECRETE_KEY
 
-const registration=expressAsyncHandler(async (req,res)=>{
+const registration = expressAsyncHandler(async (req, res) => {
     const { username, email, password } = req.body
 
     if (!username || !email || !password) {
@@ -14,25 +14,25 @@ const registration=expressAsyncHandler(async (req,res)=>{
         throw new Error("All fields are manditory")
     }
 
-    const bcrypt_password=await bcrypt.hash(password,SALT_ROUND)
+    const bcrypt_password = await bcrypt.hash(password, SALT_ROUND)
 
     const newUser = new userModel({
-        username, email, password:bcrypt_password
+        username, email, password: bcrypt_password
     })
 
-    const user=await newUser.save()
+    const user = await newUser.save()
 
-    if(user._id){
+    if (user._id) {
         res.status(201).json({
             "data": `data got saved successfully with an id : ${user._id}`
         })
-    }else{
+    } else {
         res.status(400)
         throw new Error("Invalid Data")
     }
 })
 
-const login=expressAsyncHandler(async (req,res)=>{
+const login = expressAsyncHandler(async (req, res) => {
     const { email, password } = req.body
 
     if (!email || !password) {
@@ -40,24 +40,24 @@ const login=expressAsyncHandler(async (req,res)=>{
         throw new Error("All fields are manditory")
     }
 
-    const result=await userModel.findOne({email})
+    const result = await userModel.findOne({ email })
 
-    const hashedPassword=await bcrypt.compare(password,result.password)
+    const hashedPassword = await bcrypt.compare(password, result.password)
 
-    if(email && hashedPassword){
-        const jwt_token=await jwt.sign({
-            _id:result._id,
-            u_name:result.username
-        },JWT_SECRETE_KEY)
-        res.status(200).json({jwt_token})
-    }else{
+    if (email && hashedPassword) {
+        const jwt_token = await jwt.sign({
+            _id: result._id,
+            u_name: result.username
+        }, JWT_SECRETE_KEY)
+        res.status(200).json({ jwt_token })
+    } else {
         res.status(400)
         throw new Error("email and password is incorrect !!!")
     }
 })
 
-const currentUser=(req,res)=>{
+const currentUser = (req, res) => {
     res.status(200).json(req.user.u_name)
 }
 
-export {registration,login,currentUser}
+export { registration, login, currentUser }
