1e3a959dc62c913c26ae27f2b91cf4b0a137b229
PiyushJiwane <jiwanepiyush@gmail.com>
2024-05-24 15:38:15 +0530

    server starts from here.

--- 2. Task Manager App/app.js
+++ 2. Task Manager App/app.js
@@ -1,10 +1,28 @@
-const express=require("express")
+const express = require("express")
+const taskRouter = require("./routers/taskRouter")
+const connectDB = require("./config/dbConnection")
 require("dotenv").config()
 
 const app = express()
+app.locals.title = 'My App'
+
+app.use(express.json())
+app.use("/api/v1/tasks", taskRouter)
 
 const PORT = process.env?.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`server is listening on port : ${PORT}`);
-})
+const startServer = async () => {
+    try {
+        await connectDB(process.env.CONNECTION_STRING)
+        app.listen(PORT, () => {
+            console.log(`server is listening on port : ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+startServer()
+
+console.dir(app.locals.title)
